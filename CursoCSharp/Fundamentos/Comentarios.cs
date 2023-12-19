1669d404dd8cc5277a48fe5132cb1978eec27a70
vjesus <vinicius.jesus@businessintegrator.com.br>
2023-12-19 14:40:15 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            //cuidado com com os comentarios desnecessarios
+            Console.WriteLine("Código claro é sempre melhor");
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentarios - Fundamentos", Comentarios.Executar},
+                {"Variaveis E Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferencia - Fundamentos", Inferencia.Executar}
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
